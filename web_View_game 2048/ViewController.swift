f7cb132045b785ccf67735455385b9a687f86a94
Muhammad Basalamah <muhammad@Testtests-MacBook-Pro.local>
2018-10-16 14:55:20 +0700

    Initial Commit

---
+++ web_View_game 2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  web_View_game 2048
+//
+//  Created by Muhammad Basalamah on 16/10/18.
+//  Copyright © 2018 Mubs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
